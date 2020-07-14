cf99d743c8a6369e91a6a680c6b6756364458ea0
Lime Peach <limepeach@LimePeach.local>
2020-07-13 17:33:13 -0700

    Updates

---
+++ precache-manifest.796dd81f1c6bfd94c6a437338d2ee7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0859ee717979e3f8ad9833c93830fb9",
+    "url": "/covid-map-app/index.html"
+  },
+  {
+    "revision": "bdafaea2816178d681ef",
+    "url": "/covid-map-app/static/css/main.9242ff70.chunk.css"
+  },
+  {
+    "revision": "ec3c5dec2f7e0a83b00e",
+    "url": "/covid-map-app/static/js/2.d2fd933c.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/covid-map-app/static/js/2.d2fd933c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdafaea2816178d681ef",
+    "url": "/covid-map-app/static/js/main.d3932211.chunk.js"
+  },
+  {
+    "revision": "2a6feda0b9b8e30f47e1",
+    "url": "/covid-map-app/static/js/runtime-main.f1d570b5.js"
+  }
+]);
